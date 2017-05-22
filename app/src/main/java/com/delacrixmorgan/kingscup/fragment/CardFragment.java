921b60ad8e0f1c9344a177b52efe8e596e0c6964
Delacrix Morgan <delacrixmorgan@protonmail.com>
2017-05-22 11:59:57 +0800

    Animation Added to Hint Next Button - CardFragment

--- app/src/main/java/com/delacrixmorgan/kingscup/fragment/CardFragment.java
+++ app/src/main/java/com/delacrixmorgan/kingscup/fragment/CardFragment.java
@@ -4,8 +4,12 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,7 +22,7 @@ import com.delacrixmorgan.kingscup.model.Card;
  * Created by Delacrix Morgan on 09/10/2016.
  */
 
-public class CardFragment extends Fragment {
+public class CardFragment extends Fragment implements View.OnClickListener, View.OnTouchListener {
 
     private static String TAG = "CardFragment";
 
@@ -30,6 +34,7 @@ public class CardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_card, container, false);
+        rootView.setOnTouchListener(this);
 
         mTextName = (TextView) rootView.findViewById(R.id.card_name);
         mTextAction = (TextView) rootView.findViewById(R.id.card_action);
@@ -48,7 +53,7 @@ public class CardFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Card card = GameEngine.getInstance().getCurrentCard();
+        final Card card = GameEngine.getInstance().getCurrentCard();
 
         mTextName.setText(card.getmName());
         mTextAction.setText(card.getmAction());
@@ -82,12 +87,44 @@ public class CardFragment extends Fragment {
         }
 
         mNextButton.setText(GameEngine.getInstance().getNextText());
-        mNextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        mNextButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_next:
                 getActivity().getFragmentManager().findFragmentByTag("SelectFragment").onResume();
                 getActivity().getFragmentManager().popBackStack();
-            }
-        });
+
+                break;
+        }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Animation animationScaleDown = AnimationUtils.loadAnimation(getActivity(), R.anim.pop_in);
+
+                AnimationSet animShrink = new AnimationSet(true);
+                animShrink.addAnimation(animationScaleDown);
+                mNextButton.startAnimation(animShrink);
+
+                return true;
+
+            case MotionEvent.ACTION_UP:
+                Animation animationScaleUp = AnimationUtils.loadAnimation(getActivity(), R.anim.pop_out);
+
+                AnimationSet animGrow = new AnimationSet(true);
+                animGrow.addAnimation(animationScaleUp);
+                mNextButton.startAnimation(animGrow);
+
+                return true;
+
+            default:
+                break;
+        }
+        return false;
     }
 }
