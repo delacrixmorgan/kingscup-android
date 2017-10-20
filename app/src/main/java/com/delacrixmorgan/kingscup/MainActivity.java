f2214033e45c0f6ec2657afc2c574aca9456b0ff
Delacrix Morgan <delacrixmorgan@protonmail.com>
2017-10-20 14:36:00 +0800

    Using Support Library Custom Font, Optimisation

--- app/src/main/java/com/delacrixmorgan/kingscup/MainActivity.java
+++ app/src/main/java/com/delacrixmorgan/kingscup/MainActivity.java
@@ -10,8 +10,7 @@ import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
 
 import com.delacrixmorgan.kingscup.engine.GameEngine;
 import com.delacrixmorgan.kingscup.fragment.MenuFragment;
@@ -34,21 +33,10 @@ public class MainActivity extends Activity {
         Helper.setLocaleLanguage(this);
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-                .setDefaultFontPath("fonts/LeagueSpartan-Bold.otf")
-                .setFontAttrId(R.attr.fontPath)
-                .build()
-        );
-
         getFragmentManager()
                 .beginTransaction()
                 .replace(R.id.activity_main_vg_fragment, new MenuFragment())
                 .commit();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             TypedValue typedValue = new TypedValue();
@@ -63,13 +51,22 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
-
-    @Override
     public void onBackPressed() {
         super.onBackPressed();
         GameEngine.getInstance().playSound(this, "CARD_WHOOSH");
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
