e99eaa7325b138892839d7f24b17357b9f967f3d
Delacrix Morgan <delacrixmorgan@protonmail.com>
2017-11-04 13:12:22 +0800

    Start Kotlin Conversion, Added Kotlin Extension and Reflection

---
+++ app/src/main/java/com/delacrixmorgan/kingscup/fragment/MenuFragment.kt
@@ -0,0 +1,50 @@
+package com.delacrixmorgan.kingscup.fragment
+
+import android.app.Fragment
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.delacrixmorgan.kingscup.GameActivity
+import com.delacrixmorgan.kingscup.R
+import com.delacrixmorgan.kingscup.engine.GameEngine
+import com.delacrixmorgan.kingscup.shared.Helper
+import kotlinx.android.synthetic.main.fragment_menu.*
+
+/**
+ * Created by Delacrix Morgan on 09/10/2016.
+ */
+
+class MenuFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        GameEngine.newInstance(activity)
+
+        rateButton.setOnClickListener {
+            rateButton.isEnabled = false
+        }
+
+        startButton.setOnClickListener {
+            GameEngine.newInstance(activity)
+            startActivity(Intent(activity, GameActivity::class.java))
+        }
+
+        settingButton.setOnClickListener {
+            settingButton.isEnabled = false
+            Helper.showFragment(activity, SettingFragment(), this::class.simpleName)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        rateButton.isEnabled = true
+        startButton.isEnabled = true
+        settingButton.isEnabled = true
+    }
+}
---
+++ app/src/main/java/com/delacrixmorgan/kingscup/fragment/SettingFragment.kt
@@ -0,0 +1,151 @@
+package com.delacrixmorgan.kingscup.fragment
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.preference.Preference
+import android.preference.PreferenceFragment
+import android.preference.SwitchPreference
+import android.view.View
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+import com.delacrixmorgan.kingscup.MainActivity
+import com.delacrixmorgan.kingscup.R
+import com.delacrixmorgan.kingscup.engine.GameEngine
+import com.delacrixmorgan.kingscup.shared.Helper
+
+import android.content.Context.MODE_PRIVATE
+
+/**
+ * Created by Delacrix Morgan on 04/03/2017.
+ */
+
+class SettingFragment : PreferenceFragment() {
+
+    internal val unicode = intArrayOf(0x1F648, 0x1F649, 0x1F64A)
+    private var mQuickGuide: SwitchPreference? = null
+    private var mSoundEffects: SwitchPreference? = null
+    private var mLanguage: Preference? = null
+    private var mCreditLibrary: Preference? = null
+    private var mVersion: Preference? = null
+    private var mMonkeyCounter: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.preferences)
+
+        mQuickGuide = findPreference("quick_guide") as SwitchPreference
+        mSoundEffects = findPreference("sound_effects") as SwitchPreference
+
+        mLanguage = findPreference("language")
+        mCreditLibrary = findPreference("credit_library")
+        mVersion = findPreference("version_number")
+
+        mMonkeyCounter = 0
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mQuickGuide!!.isChecked = activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).getBoolean(Helper.QUICK_GUIDE_PREFERENCE, true)
+        mQuickGuide!!.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
+            val editor = activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).edit()
+            editor.putBoolean(Helper.QUICK_GUIDE_PREFERENCE, newValue as Boolean)
+            editor.apply()
+            true
+        }
+
+        mSoundEffects!!.isChecked = activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).getBoolean(Helper.SOUND_EFFECTS_PREFERENCE, true)
+        mSoundEffects!!.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
+            val editor = activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).edit()
+            editor.putBoolean(Helper.SOUND_EFFECTS_PREFERENCE, newValue as Boolean)
+            editor.apply()
+            true
+        }
+
+        mLanguage!!.summary = Helper.mLanguageItems[activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).getInt(Helper.LANGUAGE_PREFERENCE, 0)]
+        mLanguage!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            AlertDialog.Builder(activity)
+                    .setTitle("Language")
+                    .setSingleChoiceItems(Helper.mLanguageItems, activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).getInt(Helper.LANGUAGE_PREFERENCE, 0)
+                    ) { dialog, item ->
+                        val editor = activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).edit()
+                        editor.putInt(Helper.LANGUAGE_PREFERENCE, item)
+                        editor.apply()
+
+                        mLanguage!!.summary = Helper.mLanguageItems[item]
+                        Helper.setLocaleLanguage(activity)
+
+                        val refresh = Intent(activity, MainActivity::class.java)
+                        activity.finish()
+                        startActivity(refresh)
+                    }
+                    .setNegativeButton("Cancel") { dialog, id -> dialog.dismiss() }
+                    .show()
+
+            false
+        }
+
+
+        mCreditLibrary!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            val creditDialog = Dialog(activity)
+
+            creditDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            creditDialog.setContentView(R.layout.view_credit)
+            creditDialog.setTitle("Credits")
+
+            creditDialog.show()
+
+            val spartanImageView = creditDialog.findViewById<View>(R.id.iv_spartan_github) as ImageView
+            spartanImageView.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("https://github.com/theleagueof/league-spartan")
+                startActivity(intent)
+            }
+
+            val jenkinsImageView = creditDialog.findViewById<View>(R.id.iv_jenkins_github) as ImageView
+            jenkinsImageView.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("https://github.com/chrisjenx/Calligraphy")
+                startActivity(intent)
+            }
+
+            val balysImageView = creditDialog.findViewById<View>(R.id.iv_balys_github) as ImageView
+            balysImageView.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("https://github.com/balysv/material-ripple")
+                startActivity(intent)
+            }
+
+            val doneButton = creditDialog.findViewById<View>(R.id.button_done) as Button
+            doneButton.setOnClickListener { creditDialog.dismiss() }
+
+            false
+        }
+
+        try {
+            mVersion!!.summary = activity.packageManager.getPackageInfo(activity.packageName, 0).versionName
+            mVersion!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                mMonkeyCounter = if (mMonkeyCounter > 2) 0 else mMonkeyCounter
+                Toast.makeText(activity, String(Character.toChars(unicode[mMonkeyCounter++])), Toast.LENGTH_SHORT).show()
+
+                false
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    companion object {
+        private val TAG = "SettingFragment"
+    }
+}
