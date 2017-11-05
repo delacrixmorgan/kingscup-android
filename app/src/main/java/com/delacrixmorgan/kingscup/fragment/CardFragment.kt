a1c0a606071268d68eb2b9a7249a28b2b5f7eeb9
Delacrix Morgan <delacrixmorgan@protonmail.com>
2017-11-05 17:13:57 +0800

    Optimised CardFragment Code with Kotlin Extension

--- app/src/main/java/com/delacrixmorgan/kingscup/fragment/CardFragment.kt
+++ app/src/main/java/com/delacrixmorgan/kingscup/fragment/CardFragment.kt
@@ -5,20 +5,16 @@ import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Vibrator
-import android.support.design.widget.FloatingActionButton
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationSet
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
-import android.widget.TextView
-
 import com.delacrixmorgan.kingscup.R
 import com.delacrixmorgan.kingscup.engine.GameEngine
-import com.delacrixmorgan.kingscup.model.Card
 import com.delacrixmorgan.kingscup.shared.Helper
+import kotlinx.android.synthetic.main.fragment_card.*
 
 /**
  * Created by Delacrix Morgan on 09/10/2016.
@@ -26,66 +22,45 @@ import com.delacrixmorgan.kingscup.shared.Helper
 
 class CardFragment : Fragment(), View.OnTouchListener {
 
-    private var mTextName: TextView? = null
-    private var mTextAction: TextView? = null
-    private var mTextLightValue: TextView? = null
-    private var mTextDarkValue: TextView? = null
-    private var mLightLargeSymbol: ImageView? = null
-    private var mLightSmallSymbol: ImageView? = null
-    private var mDarkSmallSymbol: ImageView? = null
-    private var mDoneButton: FloatingActionButton? = null
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_card, container, false)
-
-        mTextName = rootView.findViewById(R.id.card_name)
-        mTextAction = rootView.findViewById(R.id.card_action)
-        mTextLightValue = rootView.findViewById(R.id.card_light_value)
-        mTextDarkValue = rootView.findViewById(R.id.card_dark_value)
-
-        mLightLargeSymbol = rootView.findViewById(R.id.card_light_lg_symbol)
-        mLightSmallSymbol = rootView.findViewById(R.id.card_light_sm_symbol)
-        mDarkSmallSymbol = rootView.findViewById(R.id.card_dark_sm_symbol)
-
-        mDoneButton = rootView.findViewById(R.id.fragment_card_done_button)
-
-        return rootView
+        return inflater.inflate(R.layout.fragment_card, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val card = GameEngine.getInstance().currentCard
 
-        Helper.animateButtonGrow(activity, mDoneButton)
+        Helper.animateButtonGrow(activity, doneButton)
 
-        mTextName!!.text = card.getmName()
-        mTextAction!!.text = card.getmAction()
-        mTextLightValue!!.text = card.getmValue()
-        mTextDarkValue!!.text = card.getmValue()
+        nameTextView.text = card.getmName()
+        actionTextView.text = card.getmAction()
+
+        lightLeftTextView.text = card.getmValue()
+        darkRightTextView!!.text = card.getmValue()
 
         when (card.getmSuit()) {
             "Spade" -> {
-                mLightLargeSymbol!!.setBackgroundResource(R.drawable.spade_pink)
-                mLightSmallSymbol!!.setBackgroundResource(R.drawable.spade_pink)
-                mDarkSmallSymbol!!.setBackgroundResource(R.drawable.spade_dark)
+                lightCenterImageView.setBackgroundResource(R.drawable.spade_pink)
+                lightLeftImageView.setBackgroundResource(R.drawable.spade_pink)
+                darkRightImageView!!.setBackgroundResource(R.drawable.spade_dark)
             }
 
             "Heart" -> {
-                mLightLargeSymbol!!.setBackgroundResource(R.drawable.heart_pink)
-                mLightSmallSymbol!!.setBackgroundResource(R.drawable.heart_pink)
-                mDarkSmallSymbol!!.setBackgroundResource(R.drawable.heart_dark)
+                lightCenterImageView.setBackgroundResource(R.drawable.heart_pink)
+                lightLeftImageView.setBackgroundResource(R.drawable.heart_pink)
+                darkRightImageView!!.setBackgroundResource(R.drawable.heart_dark)
             }
 
             "Club" -> {
-                mLightLargeSymbol!!.setBackgroundResource(R.drawable.club_pink)
-                mLightSmallSymbol!!.setBackgroundResource(R.drawable.club_pink)
-                mDarkSmallSymbol!!.setBackgroundResource(R.drawable.club_dark)
+                lightCenterImageView.setBackgroundResource(R.drawable.club_pink)
+                lightLeftImageView.setBackgroundResource(R.drawable.club_pink)
+                darkRightImageView!!.setBackgroundResource(R.drawable.club_dark)
             }
 
             "Diamond" -> {
-                mLightLargeSymbol!!.setBackgroundResource(R.drawable.diamond_pink)
-                mLightSmallSymbol!!.setBackgroundResource(R.drawable.diamond_pink)
-                mDarkSmallSymbol!!.setBackgroundResource(R.drawable.diamond_dark)
+                lightCenterImageView.setBackgroundResource(R.drawable.diamond_pink)
+                lightLeftImageView.setBackgroundResource(R.drawable.diamond_pink)
+                darkRightImageView!!.setBackgroundResource(R.drawable.diamond_dark)
             }
         }
 
@@ -94,14 +69,14 @@ class CardFragment : Fragment(), View.OnTouchListener {
             val vibrator = activity.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
             vibrator.vibrate(2000)
 
-            mDoneButton!!.visibility = View.GONE
-            mDoneButton!!.isEnabled = false
+            doneButton.visibility = View.GONE
+            doneButton.isEnabled = false
 
             val handler = Handler()
             handler.post {
                 val animGrow = AnimationSet(true)
                 animGrow.addAnimation(AnimationUtils.loadAnimation(activity, R.anim.pop_out))
-                mLightLargeSymbol!!.startAnimation(animGrow)
+                lightCenterImageView.startAnimation(animGrow)
             }
 
             handler.postDelayed({
@@ -112,8 +87,8 @@ class CardFragment : Fragment(), View.OnTouchListener {
             }, 2000)
 
         } else {
-            mDoneButton!!.setOnTouchListener(this)
-            mDoneButton!!.isEnabled = true
+            doneButton.setOnTouchListener(this)
+            doneButton.isEnabled = true
         }
     }
 
