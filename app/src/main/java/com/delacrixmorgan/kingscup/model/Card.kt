5e569d1e778af895ffe68fda4f3b00a6ec40f4bf
Delacrix Morgan <delacrixmorgan@protonmail.com>
2017-11-11 12:16:46 +0800

    Convert Card Model to Kotlin Data Class

---
+++ app/src/main/java/com/delacrixmorgan/kingscup/model/Card.kt
@@ -0,0 +1,7 @@
+package com.delacrixmorgan.kingscup.model
+
+/**
+ * Created by Delacrix Morgan on 09/10/2016.
+ */
+
+data class Card(val suit: String, val rank: String, val header: String, val body: String)
