8694abe753074660443243dc359a5fe83101d216
Delacrix Morgan <delacrixmorgan@protonmail.com>
2017-11-24 00:13:10 +0800

    Able to switch between different languages without restarting the activity

--- app/src/main/java/com/delacrixmorgan/kingscup/menu/MenuSettingFragment.kt
+++ app/src/main/java/com/delacrixmorgan/kingscup/menu/MenuSettingFragment.kt
@@ -5,18 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.delacrixmorgan.kingscup.R
 import kotlinx.android.synthetic.main.fragment_menu_setting.*
+import java.util.*
 
 /**
  * Created by Delacrix Morgan on 04/03/2017.
  */
 
 class MenuSettingFragment : Fragment() {
-
     companion object {
         fun newInstance(): MenuSettingFragment = MenuSettingFragment()
     }
+
+    val languageList = arrayListOf("en", "zh")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
             inflater.inflate(R.layout.fragment_menu_setting, container, false)
@@ -27,6 +30,92 @@ class MenuSettingFragment : Fragment() {
         this.backButton.setOnClickListener {
             fragmentManager.popBackStack()
         }
+
+        this.languageTextView.setOnClickListener {
+            this.changeLanguage()
+        }
+    }
+
+    private fun changeLanguage() {
+        val currentLanguage = Locale.getDefault().language
+        val locale = Locale(when (currentLanguage) {
+            languageList[0] -> languageList[1]
+            languageList[1] -> languageList[0]
+            else -> languageList[0]
+        })
+
+        with(resources) {
+            configuration.setLocale(locale)
+            configuration.setLayoutDirection(locale)
+            updateConfiguration(configuration, displayMetrics)
+            onConfigurationChanged(configuration)
+        }
+
+        this.updateSettingLanguage()
+        Locale.setDefault(locale)
+
+        Toast.makeText(activity, "Language Updated", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updateSettingLanguage() {
+        this.creditTextView.text = activity.getString(R.string.preference_credits_summary)
+        this.shareTextView.text = activity.getString(R.string.preference_share_friend)
+        this.languageTextView.text = Locale.getDefault().language
+    }
+
+//    @SuppressLint("ObsoleteSdkInt")
+//    private fun setLocale(locale: Locale) {
+//        val resources = resources
+//        val configuration = resources.configuration
+//        val displayMetrics = resources.displayMetrics
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+//            configuration.setLocale(locale)
+//
+//            activity.createConfigurationContext(configuration)
+//
+//
+//            Toast.makeText(activity, "Language Updated", Toast.LENGTH_SHORT).show()
+//        } else {
+//            configuration.locale = locale
+//            resources.updateConfiguration(configuration, displayMetrics)
+//        }
+//
+//        Locale.setDefault(locale)
+//    }
+
+    private fun legacyCode() {
+//        mLanguage!!.summary = Helper.mLanguageItems[activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).getInt(Helper.LANGUAGE_PREFERENCE, 0)]
+//        mLanguage!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+//            AlertDialog.Builder(activity)
+//                    .setTitle("Language")
+//                    .setSingleChoiceItems(Helper.mLanguageItems, activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).getInt(Helper.LANGUAGE_PREFERENCE, 0)
+//                    ) { dialog, item ->
+//                        val editor = activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).edit()
+//                        editor.putInt(Helper.LANGUAGE_PREFERENCE, item)
+//                        editor.apply()
+//
+//                        mLanguage!!.summary = Helper.mLanguageItems[item]
+//        var languageCode = Locale.getDefault().language
+//
+//        when (context.getSharedPreferences(Helper.SHARED_PREFERENCE, Context.MODE_PRIVATE).getInt(Helper.LANGUAGE_PREFERENCE, 1)) {
+//            1 -> languageCode = "en"
+//            2 -> languageCode = "zh"
+//        }
+//
+//        val configuration = Configuration(context.resources.configuration)
+//        configuration.locale = Locale(languageCode)
+//        context.resources.updateConfiguration(configuration, context.resources.displayMetrics)
+//
+//                        GameEngine.newInstance(activity)
+//                        val refresh = Intent(activity, MainActivity::class.java)
+//                        activity.finish()
+//                        startActivity(refresh)
+//                    }
+//                    .setNegativeButton("Cancel") { dialog, id -> dialog.dismiss() }
+//                    .show()
+//
+//            false
+//        }
 
 //        mQuickGuide!!.isChecked = activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).getBoolean(Helper.QUICK_GUIDE_PREFERENCE, true)
 //        mQuickGuide!!.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
@@ -44,29 +133,7 @@ class MenuSettingFragment : Fragment() {
 //            true
 //        }
 //
-//        mLanguage!!.summary = Helper.mLanguageItems[activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).getInt(Helper.LANGUAGE_PREFERENCE, 0)]
-//        mLanguage!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
-//            AlertDialog.Builder(activity)
-//                    .setTitle("Language")
-//                    .setSingleChoiceItems(Helper.mLanguageItems, activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).getInt(Helper.LANGUAGE_PREFERENCE, 0)
-//                    ) { dialog, item ->
-//                        val editor = activity.getSharedPreferences(Helper.SHARED_PREFERENCE, MODE_PRIVATE).edit()
-//                        editor.putInt(Helper.LANGUAGE_PREFERENCE, item)
-//                        editor.apply()
 //
-//                        mLanguage!!.summary = Helper.mLanguageItems[item]
-//                        Helper.setLocaleLanguage(activity)
-//
-//                        GameEngine.newInstance(activity)
-//                        val refresh = Intent(activity, MainActivity::class.java)
-//                        activity.finish()
-//                        startActivity(refresh)
-//                    }
-//                    .setNegativeButton("Cancel") { dialog, id -> dialog.dismiss() }
-//                    .show()
-//
-//            false
-//        }
 //
 //
 //        mCreditLibrary!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
