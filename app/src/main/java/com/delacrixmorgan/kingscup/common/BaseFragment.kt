7f58232311c4aa3dff079ef6d172dd165570262d
Delacrix Morgan <delacrixmorgan@protonmail.com>
2018-01-14 15:26:58 +0800

    Move baseActivity and baseContext set to onAttach LifeCycle

--- app/src/main/java/com/delacrixmorgan/kingscup/common/BaseFragment.kt
+++ app/src/main/java/com/delacrixmorgan/kingscup/common/BaseFragment.kt
@@ -1,7 +1,6 @@
 package com.delacrixmorgan.kingscup.common
 
 import android.content.Context
-import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 
@@ -13,8 +12,8 @@ open class BaseFragment : Fragment() {
     lateinit var baseActivity: AppCompatActivity
     lateinit var baseContext: Context
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
 
         activity?.let { this.baseActivity = it as AppCompatActivity }
         context?.let { this.baseContext = it }
