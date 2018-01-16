a0fcb2d33a0a35b2dd64a70b662a9da8b75dff7c
Delacrix Morgan <delacrixmorgan@protonmail.com>
2018-01-16 23:06:34 +0800

    Add changeAppOverview to have logo with no background

--- app/src/main/java/com/delacrixmorgan/kingscup/MainActivity.kt
+++ app/src/main/java/com/delacrixmorgan/kingscup/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.delacrixmorgan.kingscup
 
 import android.app.ActivityManager
+import android.content.res.Resources
 import android.graphics.BitmapFactory
 import android.media.AudioManager
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
 import com.delacrixmorgan.kingscup.common.SoundEngine
@@ -27,6 +29,8 @@ class MainActivity : AppCompatActivity() {
 
         SoundEngine.newInstance(this)
         volumeControlStream = AudioManager.STREAM_MUSIC
+
+        this.changeAppOverview(this, theme)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -45,6 +49,17 @@ class MainActivity : AppCompatActivity() {
                     or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_FULLSCREEN
                     or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+    }
+
+    private fun changeAppOverview(activity: AppCompatActivity, theme: Resources.Theme) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val typedValue = TypedValue()
+            val colour = typedValue.data
+            val bitmap = BitmapFactory.decodeResource(activity.resources, R.drawable.kingscup_logo_no_bg)
+            activity.setTaskDescription(ActivityManager.TaskDescription(null, bitmap, colour))
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true)
+            bitmap.recycle()
         }
     }
 }
