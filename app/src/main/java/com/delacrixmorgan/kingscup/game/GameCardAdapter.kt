af3e429d4f29605e89bdd48ef365ae4e66d17d46
Delacrix Morgan <delacrixmorgan@protonmail.com>
2018-01-16 23:03:01 +0800

    Add fix to adapterSize changes

--- app/src/main/java/com/delacrixmorgan/kingscup/game/GameCardAdapter.kt
+++ app/src/main/java/com/delacrixmorgan/kingscup/game/GameCardAdapter.kt
@@ -11,20 +11,23 @@ import com.delacrixmorgan.kingscup.R
  * Created by Delacrix Morgan on 17/03/2017.
  **/
 
-class GameCardAdapter(private val selectionListener: CardListener, private val deckSize: Int) : RecyclerView.Adapter<GameCardAdapter.GameCardViewHolder>() {
+class GameCardAdapter(private val selectionListener: CardListener, private var deckSize: Int) : RecyclerView.Adapter<GameCardAdapter.GameCardViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameCardViewHolder {
         return GameCardViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.view_game_card, parent, false))
     }
 
     override fun onBindViewHolder(holder: GameCardViewHolder, position: Int) {
-        holder.selectCard.setOnClickListener { this.selectionListener.onCardSelected(holder.adapterPosition) }
+        holder.selectCard.setOnClickListener {
+            this.deckSize--
+            this.selectionListener.onCardSelected(holder.adapterPosition)
+        }
     }
 
     override fun getItemCount(): Int {
-        return deckSize
+        return this.deckSize
     }
-    
+
     class GameCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var selectCard: LinearLayout = itemView.findViewById(R.id.view_card_layout)
     }
