e2918b122f1a4946b34ff68d835cc0faf755d505
Delacrix Morgan <delacrixmorgan@protonmail.com>
2018-02-08 22:50:01 +0800

    Create LanguageType enums

---
+++ app/src/main/java/com/delacrixmorgan/kingscup/common/LanguageType.kt
@@ -0,0 +1,11 @@
+package com.delacrixmorgan.kingscup.common
+
+/**
+ * Created by Delacrix Morgan on 08/02/2018.
+ **/
+
+enum class LanguageType(var countryIso: String) {
+    ENGLISH("en"),
+    CHINESE("zh_Hans"),
+    PORTUGUESE_BRAZIL("pt-rBR")
+}
