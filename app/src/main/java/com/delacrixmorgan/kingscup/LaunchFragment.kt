21e9cc52809138b981c35739233f66d1cc7c6e5c
Morgan Koh <delacrixmorgan@pm.me>
2019-05-08 19:19:06 +0800

    Add LaunchFragment

---
+++ app/src/main/java/com/delacrixmorgan/kingscup/LaunchFragment.kt
@@ -0,0 +1,25 @@
+package com.delacrixmorgan.kingscup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * LaunchFragment
+ * kingscup-android
+ *
+ * Created by Delacrix Morgan on 08/05/2018.
+ * Copyright (c) 2019 licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+ */
+
+class LaunchFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_launch, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
