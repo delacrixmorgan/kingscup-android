e365e90099e686429704628948bd4c5738cf1814
Morgan Koh <delacrixmorgan@pm.me>
2019-05-14 15:48:09 +0800

    Redesign MenuSettings

--- app/src/main/java/com/delacrixmorgan/kingscup/menu/MenuSettingFragment.kt
+++ app/src/main/java/com/delacrixmorgan/kingscup/menu/MenuSettingFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.delacrixmorgan.kingscup.R
 import com.delacrixmorgan.kingscup.common.launchShareGameIntent
+import com.delacrixmorgan.kingscup.common.launchWebsite
 import kotlinx.android.synthetic.main.fragment_menu_setting.*
 
 /**
@@ -30,23 +31,27 @@ class MenuSettingFragment : Fragment() {
         this.backButton.setOnClickListener {
             Navigation.findNavController(view).navigateUp()
         }
-
-        this.guideViewGroup.setOnClickListener {
+        
+        this.guideCardView.setOnClickListener {
             val action = MenuSettingFragmentDirections.actionMenuSettingFragmentToMenuGuideFragment()
             Navigation.findNavController(view).navigate(action)
         }
 
-        this.creditViewGroup.setOnClickListener {
-            val action = MenuSettingFragmentDirections.actionMenuSettingFragmentToMenuCreditsFragment()
-            Navigation.findNavController(view).navigate(action)
-        }
-
-        this.languageButton.setOnClickListener {
+        this.languageCardView.setOnClickListener {
             val action = MenuSettingFragmentDirections.actionMenuSettingFragmentToMenuLanguageFragment()
             Navigation.findNavController(view).navigate(action)
         }
 
-        this.shareViewGroup.setOnClickListener {
+        this.creditsCardView.setOnClickListener {
+            val action = MenuSettingFragmentDirections.actionMenuSettingFragmentToMenuCreditsFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
+
+        this.sourceCodeCardView.setOnClickListener {
+            launchWebsite("https://github.com/delacrixmorgan/kingscup-android")
+        }
+
+        this.shareCardView.setOnClickListener {
             val message = getString(R.string.preference_message_share_friend)
             launchShareGameIntent(message)
         }
