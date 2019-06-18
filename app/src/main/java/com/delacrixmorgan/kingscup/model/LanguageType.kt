81a4c9401c16a0355298330ba97045623f885b12
Morgan Koh <delacrixmorgan@pm.me>
2019-06-18 18:08:50 +0800

    Add French in LanguageType

--- app/src/main/java/com/delacrixmorgan/kingscup/model/LanguageType.kt
+++ app/src/main/java/com/delacrixmorgan/kingscup/model/LanguageType.kt
@@ -14,9 +14,6 @@ enum class LanguageType(val countryIso: String, val authorNames: String) {
     PORTUGUESE("pt", "Lays Correia"),
     DUTCH("nl", "Kasper Nooteboom"),
     SPANISH("es", "Santos Martinez\nGonzo Fernandez"),
-    FINNISH("fi", "Karim Moubarik");
-}
-
-fun LanguageType.formatText(): String {
-    return this.name.toLowerCase().capitalize()
+    FINNISH("fi", "Karim Moubarik"),
+    FRENCH("fr", "David Chitchong Thingee")
 }
